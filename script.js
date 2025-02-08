1e561f2334fc21a4b5ef8cf78155777fec6b915f
Ashish <ashishmasih92g@gmail.com>
2025-02-08 23:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let list = document.querySelectorAll("li")
+let div = document.querySelector("div");
+let heading = document.querySelector("h1");
+heading.innerText="Welcome to Accio Jobs!"
+
+let para = document.createElement("p");
+	para.innerText="5 software program developer profession paths";
+
+div.append(para);
+
+let span = document.createElement("span")
+let v = span.innerText=" development"
+
+for(let t of list){
+	t.append(v)
+}
+
+
+
